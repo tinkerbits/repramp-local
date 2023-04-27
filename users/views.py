5ca1ae5922a3ecb7768b1d92bfbb47fa485f6574
tinkerbits <jon.burke.contact@gmail.com>
2023-04-27 21:53:06 +0200

    added Register New User functionality

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.views.generic import CreateView
+
+from .forms import CustomUserCreationForm
+
+
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'register.html'
+
