65fb5013708b22ce4337717a3eaf56007f6576a0
tinkerbits <jon.burke.contact@gmail.com>
2023-06-12 07:34:02 +0200

    finish layouting of managers/manage-email-addresses

--- managers/forms.py
+++ managers/forms.py
@@ -28,9 +28,12 @@ class RegisterNewUserForm(CustomUserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['role'].choices = [
-            ('warmupper', 'warmupper'),
             ('sender', 'sender'),
+            ('warmupper', 'warmupper'),
         ]
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-solid border-2 p-1 bg-slate-600 w-full'
+            field.widget.attrs['placeholder'] = field.label
 
 class AddEmailAddressForm(forms.ModelForm):
     class Meta:
